7a2d61288918c81c5f16e126ee8d6e3c7ae16145
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-11 16:01:41 +0900

    function session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :member, :profile, :works, :avatar])
   end
 
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
 end
