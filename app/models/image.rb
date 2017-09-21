cba50ea038daf64301b69024ef5cd486eafcfbbe
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-22 00:22:29 +0900

    add association

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,5 @@
 class Image < ActiveRecord::Base
+
+  belongs_to :prototype
+
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,7 @@
 class Prototype < ActiveRecord::Base
+
+  has_many :images
+  has_many :likes
+  has_many :comments
+
 end
