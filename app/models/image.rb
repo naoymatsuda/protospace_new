e5b7b79ed09b73c0cdc8e22e9d09ef7014e17436
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-22 00:33:55 +0900

    add validates to model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,7 @@
 class Image < ActiveRecord::Base
+  validates :image, presence: true
 
   belongs_to :prototype
+  mount_uploader :image
 
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,13 @@
 class Prototype < ActiveRecord::Base
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
 
   has_many :images
   has_many :likes
   has_many :comments
+
+  belongs_to :user
 
 end
