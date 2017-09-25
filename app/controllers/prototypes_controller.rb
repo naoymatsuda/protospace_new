24f7e2b2f4244272505facfb1b22177bd0362733
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-25 21:58:39 +0900

    write contoroller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,4 +3,22 @@ class PrototypesController < ApplicationController
   def index
   end
 
+  def new
+  	@prototype = Prototype.new
+  	@prototype.images.build
+  end
+
+  def create
+  	@prototypes = Prototype.new(prototype_params)
+  	if @prototypes.save
+  	  redirect_to root_path, notice: 'Your post is success'
+  	else
+  	  render action: :new
+  	  redirect_to new_prototype_path, alert: 'Your post is unsuccessful'
+    end
+  end
+
+  def prototype_params
+  	params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:id, :status, :image]).merge(user_id: current_user.id)
+  end
 end
