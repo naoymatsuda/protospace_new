83c9598ebf9af5ab77008d551b754d4410c63cbf
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-25 21:53:42 +0900

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes, only: [:index]
+  resources :prototypes
   resources :users, only: [:show, :edit, :update]
 end
