b33806941e688da94b7b53bc28696455d3d28dbc
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-28 12:07:34 +0900

    correct indent error

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,21 +4,21 @@ class PrototypesController < ApplicationController
   end
 
   def new
-  	@prototype = Prototype.new
-  	@prototype.images.build
+    @prototype = Prototype.new
+    @prototype.images.build
   end
 
   def create
-  	@prototype = Prototype.new(prototype_params)
-  	if @prototype.save
-  	  redirect_to root_path, notice: ' Post was successful '
-  	else
-  	  redirect_to new_prototype_path, alert: ' Post was unsuccessful '
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path, notice: ' Post was successful '
+    else
+      redirect_to new_prototype_path, alert: ' Post was unsuccessful '
     end
   end
 
   def prototype_params
-  	params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:image, :status, :id]).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:image, :status, :id]).merge(user_id: current_user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes
+  resources :prototypes, only: [:new, :create, :index])
   resources :users, only: [:show, :edit, :update]
 end
