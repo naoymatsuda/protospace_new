f662ab5d18f4b2ef5974758eed6be10f9a30bd97
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-29 22:53:34 +0900

    set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes, only: [:new, :create, :index, :show]
+  resources :prototypes, only: [:new, :create, :index, :show] do
+    resources :comments, only: [:new, :create]
+  end
   resources :users, only: [:show, :edit, :update]
 end
