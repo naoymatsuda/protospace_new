dfbf6a7f2fd2688037a5789034320dcbd53ba2a1
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-29 22:56:45 +0900

    make controller
    コメントコントローラーを作成し、実装を行なった。

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @prototype = Prototype.find(params[:prototype_id])
+    @comment = Comment.new
+    @comments = Comment.where(prototype_id: params[:prototype_id])
+    @new_comment = current_user.comments.create(text: comment_params, prototype_id: params[:prototype_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)[:text]
+  end
+
+end
