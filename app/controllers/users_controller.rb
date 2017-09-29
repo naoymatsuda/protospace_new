ac838e5f95112eded3b13477a6147c71098d901b
naoymatsuda <naoy.matsuda@gmail.com>
2017-09-29 14:51:01 +0900

    入れ替え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
     @prototypes = @user.prototypes
+    @user = User.find(params[:id])
   end
 
   def edit
