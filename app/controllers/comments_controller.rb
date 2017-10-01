d2ff2b841991fca06de1a54a6c42de2a04a1f98a
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-01 09:11:16 +0900

    In case of failure send message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,12 @@ class CommentsController < ApplicationController
     @comment = Comment.new
     @comments = Comment.where(prototype_id: params[:prototype_id])
     @new_comment = current_user.comments.create(text: comment_params, prototype_id: params[:prototype_id])
+    if @new_comment.save
+      redirect_to root_path, notice: 'メッセージを送信しました'
+    else
+      flash[:alert] = 'メッセージの送信に失敗しました'
+      redirect_to root_path
+    end
   end
 
   def comment_params
