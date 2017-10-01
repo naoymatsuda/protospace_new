7bd8bf8e8fa7a033423ea02453679e6002ba3f20
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-01 20:25:38 +0900

    add error message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,12 +8,8 @@ class CommentsController < ApplicationController
     @comment = Comment.new
     @comments = Comment.where(prototype_id: params[:prototype_id])
     @new_comment = current_user.comments.create(text: comment_params, prototype_id: params[:prototype_id])
-    if @new_comment.save
-      redirect_to root_path, notice: 'メッセージを送信しました'
-    else
-      flash[:alert] = 'メッセージの送信に失敗しました'
-      redirect_to root_path
-    end
+    @new_comment = current_user.comments.build(text: comment_params, prototype_id: params[:prototype_id])
+    (render json: { message: 'メッセージの送信に失敗しました' } and return) unless @new_comment.save
   end
 
   def comment_params
