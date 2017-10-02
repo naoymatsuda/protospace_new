c6abaea7d23b48fe91f7e8f7911f4b44a45ff089
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-02 20:21:15 +0900

    add routes of likes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   resources :prototypes, only: [:new, :create, :index, :show] do
     resources :comments, only: [:new, :create]
+    resources :likes, only: [:new, :create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
