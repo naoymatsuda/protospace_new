ea39c1f7f13619503e798445d101feb9924ccd49
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-02 20:44:22 +0900

    correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   resources :prototypes, only: [:new, :create, :index, :show] do
     resources :comments, only: [:new, :create]
-    resources :likes, only: [:new, :create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
