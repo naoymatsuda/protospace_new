c6379c2c61b73e930a0c24e3fef187bfd25b4add
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-02 20:23:17 +0900

    make likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,18 @@
+class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.create(prototype_id: params[:prototype_id])
+    like_set
+  end
+
+  def destroy
+    @like = Like.find(params[:id]).destroy
+    like_set
+  end
+
+  private
+  def like_set
+    @prototype = Prototype.find(params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
+    @find_like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
