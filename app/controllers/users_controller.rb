a55ea76d78d126b5cddfd62e6a61717972ac19de
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-03 22:51:57 +0900

    use preload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    @user = User.preload(:prototypes).find(params[:id])
     @prototypes = @user.prototypes
   end
 
