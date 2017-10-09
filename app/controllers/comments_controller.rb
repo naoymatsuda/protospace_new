043f83368069cbce8d17c8055ff7f08eb9eb918c
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-09 13:01:50 +0900

    delete extara method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,9 +6,9 @@ class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
     @comment = Comment.new
-    @comments = Comment.where(prototype_id: params[:prototype_id])
+    @comments = @prototype.comments
     @new_comment = current_user.comments.build(text: comment_params, prototype_id: params[:prototype_id])
-    (render json: { message: 'メッセージの送信に失敗しました' } and return) unless @new_comment.save
+    (render json: { message: 'メッセージの送信に失敗しました' }) unless @new_comment.save
   end
 
   def comment_params
