d95f7f5505d935a52f9df47d43844e6049540977
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-10 20:48:50 +0900

    delete extra variable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,8 @@ class CommentsController < ApplicationController
 
   def create
     @prototype = Prototype.find(params[:prototype_id])
-    @comment = Comment.new
     @comments = @prototype.comments
-    @new_comment = current_user.comments.build(text: comment_params, prototype_id: params[:prototype_id])
+    @comment = current_user.comments.build(text: comment_params, prototype_id: params[:prototype_id])
     (render json: { message: 'メッセージの送信に失敗しました' }) unless @new_comment.save
   end
 
