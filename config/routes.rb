38d51e0a046d233327a05afc6398ba77863628f2
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-15 19:33:11 +0900

    set new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  get 'prototypes_latests/index'
+
   devise_for :users
   root 'prototypes#index'
   resources :prototypes, only: [:new, :create, :index, :show] do
     resources :comments, only: [:new, :create]
-    collection do
-      get 'newest'
-    end
     resources :likes, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
+  resources :prototypes_latests, only: [:index]
 end
