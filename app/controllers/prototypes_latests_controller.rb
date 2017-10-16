990d7ecffee91e02dd7868d94c13260b3bf1475b
naoymatsuda <naoy.matsuda@gmail.com>
2017-10-16 17:33:34 +0900

    delete prototypes_latests_controller

--- app/controllers/prototypes_latests_controller.rb
+++
@@ -1,6 +0,0 @@
-class PrototypesLatestsController < ApplicationController
-  def index
-    @prototypes = Prototype.includes(:images).order('created_at DESC')
-    render template: "prototypes/index"
-  end
-end
